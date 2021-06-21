85eb7c16cf32c2b3b403161a45a85ee10b1f45d5
hoyong.jang <lazytort@hotmail.com>
2021-06-21 15:44:48 +0900

    delivery man solved

--- DeliveryMan.cpp
+++ DeliveryMan.cpp
@@ -2,36 +2,96 @@
 #include<bits/stdc++.h>
 
 const int max_n = 100001;
-long long total_tips[max_n][2];
 
-long long findMaxTips(int N, long long A[], long long B[], int X, int Y)
+long findMaxTipsDP(int N, long A[], long B[], int X, int Y)
 {
-    //total_tips[0][0] = {0LL};
-    //total_tips[0][1] = {0LL};
+    long total_tips[N+1][N+1];
+    long max_tip = 0L;
 
-    long long max_tip = 0LL;
+    total_tips[0][0] = 0L;
+    for(int i=1;i<=N;++i)
+    {
+        if(i <= Y)
+            total_tips[i][0] = total_tips[i-1][0] + B[i-1];
+        else
+            total_tips[i][0] = total_tips[i-1][0];
+    }
+    for(int i=0;i<=X;++i)
+        total_tips[0][i] = 0L;
 
     for(int i=1;i<=N;++i)
     {
-        //for(int j=0;j<2;++j)
+        for(int j=1;j<=X;++j)
         {
-            //max_tip = std::max(max_tip + A[i-1], max_tip + B[i-1]);
-            long long selectA = max_tip;
-            long long selectB = max_tip;
-            if(X > 0) selectA = max_tip + A[i-1];
-            if(Y > 0) selectB = max_tip + B[i-1];
-            if(selectA > selectB)
+            long prev_A = total_tips[i-1][j-1] + A[i-1];
+            long prev_B = total_tips[i-1][j] + B[i-1];
+
+            //if(i-j > Y) total_tips[i][j] = prev_A;
+            if(prev_A > prev_B) total_tips[i][j] = prev_A;
+            else total_tips[i][j] = prev_B;
+        }
+    }
+
+    for(int j=0;j<=X;++j)
+    {
+        max_tip = std::max(max_tip, total_tips[N][j]);
+    }
+
+    return max_tip;
+}
+
+bool compare(std::pair<long, long> A, std::pair<long, long> B)
+{
+    return A.first > B.first;
+}
+
+long findMaxTipsGreedy(int N, long A[], long B[], int X, int Y)
+{
+    long max_tip = 0L;
+    std::vector<std::pair<long, long>> tip_diff;
+
+    for(int i=0;i<N;++i)
+    {
+        std::pair<long, long> diff = std::make_pair(std::abs(A[i] - B[i]), i);
+        tip_diff.push_back(diff);
+    }
+    std::sort(tip_diff.begin(), tip_diff.end(), compare);
+
+    int x = 0, y = 0;
+    long sum = 0L;
+    for(int i=0;i<N;++i)
+    {
+        //printf("%ld\n", tip_diff[i].first);
+        long idx = tip_diff[i].second;
+        if(A[idx] > B[idx])
+        {
+            if(x + 1 <= X)
             {
-                max_tip = selectA;
-                X--;
+                x++;
+                sum += A[idx];
             }
             else
             {
-                max_tip = selectB;
-                Y--;
+                y++;
+                sum += B[idx];
+            }
+        }
+        else
+        {
+            if(y + 1 <= Y)
+            {
+                y++;
+                sum += B[idx];
+            }
+            else
+            {
+                x++;
+                sum += A[idx];
             }
         }
     }
+    max_tip = sum;
+
     return max_tip;
 }
 
@@ -39,16 +99,16 @@ int main(void)
 {
     int N, X, Y;
 
-    long long A[max_n] = {0LL};
-    long long B[max_n] = {0LL};
+    long A[max_n] = {0L};
+    long B[max_n] = {0L};
 
     scanf("%d %d %d\n", &N, &X, &Y);
     for(int n=0;n<N;++n)
-        scanf("%lld ", &A[n]);
+        scanf("%ld ", &A[n]);
 
     for(int n=0;n<N;++n)
-        scanf("%lld ", &B[n]);
-    printf("%lld\n", findMaxTips(N, A, B, X, Y));
+        scanf("%ld ", &B[n]);
+    printf("%ld\n", findMaxTipsGreedy(N, A, B, X, Y));
     
     return 0;
 }
